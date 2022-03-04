4340f19a854c6ec743b80b0f2cf319dc1f397202
tomer <tomer1812@gmail.com>
2022-03-05 01:25:45 +0200

    initial commit

--- src/pydts/utils.py
+++ src/pydts/utils.py
@@ -2,9 +2,18 @@ import pandas as pd
 
 
 def get_expanded_df(df, event_type_col='J', time_col='X', pid_col='pid'):
-    '''
-    Expand df
-    '''
+    """
+    This function gets a dataframe describing each sample the time of the observed events,
+    and returns an expanded dataframe as explained in TODO add reference
+    Right censoring is allowed and must be marked as event type 0.
+
+    :param df: original dataframe (pd.DataFrame)
+    :param event_type_col: event type column name (str)
+    :param time_col: time column name (str)
+    :param pid_col: patient id column name (str)
+
+    :return: result_df: expanded dataframe
+    """
     result_df = df.reindex(df.index.repeat(df[time_col]))
     result_df[time_col] = result_df.groupby(level=0).cumcount()+1
     last_idx = result_df.drop_duplicates(subset=[pid_col], keep='last').index
