9fa96d5b456dbcd19b0294e1f7398c5ef70fdaf2
tomer <tomer1812@gmail.com>
2022-03-08 22:37:15 +0200

    e2e tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,24 @@
-def test_all():
-    pass
+import unittest
+from pydts.examples_utils.generate_simulations_data import generate_quick_start_df
+from pydts.fitters import DataExpansionFitter, TwoStagesFitter
 
 
-if __name__ == "__main__":
-    test_all()
+class TestFitters(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.df = generate_quick_start_df(n_patients=1000, n_cov=5, d_times=30, j_events=2, pid_col='pid', seed=0)
+
+    def test_e2e_DataExpansionFitter(cls):
+        m = DataExpansionFitter()
+        m.fit(df=cls.df.drop(['C', 'T'], axis=1))
+        m.print_summary()
+
+    def test_e2e_TwoStagesFitter(cls):
+        m2 = TwoStagesFitter()
+        m2.fit(cls.df)
+        m2.print_summary()
+
+
+if __name__ == '__main__':
+    unittest.main()
