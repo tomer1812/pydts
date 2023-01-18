6873df929d1c70be90cb7a88297790e4cb4c2d96
tomer <tomer1812@gmail.com>
2023-01-18 12:52:40 +0200

    auc evaluation version 0.5.6

--- src/pydts/evaluation.py
+++ src/pydts/evaluation.py
@@ -4,13 +4,13 @@ import warnings
 pd.set_option("display.max_rows", 500)
 warnings.filterwarnings('ignore')
 slicer = pd.IndexSlice
-from typing import Optional, List, Union
+from lifelines.fitters.kaplan_meier_fitter import KaplanMeierFitter
 
 
 def event_specific_prediction_error(pred_df: pd.DataFrame,
                                     event: int,
                                     event_type_col: str = 'J',
-                                    duration_col: str = 'X') -> pd.DataFrame:
+                                    duration_col: str = 'X') -> pd.Series:
     """
     This function implements the calculation of the cause-specific prediction error (PE).
 
@@ -72,3 +72,150 @@ def prediction_error(pred_df: pd.DataFrame,
                                                                       duration_col=duration_col)],
                                      axis=1)
     return prediction_error.T
+
+
+def event_specific_auc_at_t(pred_df: pd.DataFrame,
+                            event: int,
+                            t: int,
+                            event_type_col: str = 'J',
+                            duration_col: str = 'X') -> float:
+    """
+    This function implements the calculation of the event specific AUC at time t.
+
+    Args:
+        pred_df (pd.DataFrame): Data to calculate prediction error.
+                                Must contain the observed duration and event-type, and the probability of event at time t prediction results for the event.
+                                See TwoStagesFitter.predict_prob_events()
+        event (int): Event-type to calculate the integrated AUC for.
+        t (int): time to calculate the AUC for.
+        duration_col (str): Last follow up time column name (must be a column in pred_df).
+        event_type_col (str): The event type column name (must be a column in df),
+                              Right-censored sample (i) is indicated by event value 0, df.loc[i, event_type_col] = 0.
+    Returns:
+        result (pd.Series): event specific AUC for all times included in duration_col of pred_df.
+    """
+
+    event_observed_at_t_df = pred_df[(pred_df[event_type_col] == event) & (pred_df[duration_col] == t)]
+    no_event_at_t_df = pred_df[pred_df[duration_col] >= t]
+    no_event_at_t_df = no_event_at_t_df[~((no_event_at_t_df[event_type_col] == event) &
+                                          (no_event_at_t_df[duration_col] == t))]
+    total_t = (len(event_observed_at_t_df)*len(no_event_at_t_df))
+    correct_order = 0
+    for i_idx, i_row in event_observed_at_t_df.iterrows():
+        pi_ij = i_row.loc[f'prob_j{event}_at_t{t}']
+        pi_mj = no_event_at_t_df.loc[:, f'prob_j{event}_at_t{t}']
+        correct_order += ((pi_ij > pi_mj).sum()+0.5*(pi_ij == pi_mj).sum())
+    return correct_order / total_t
+
+
+def event_specific_auc_at_t_all(pred_df: pd.DataFrame,
+                                event: int,
+                                event_type_col: str = 'J',
+                                duration_col: str = 'X') -> pd.Series:
+    """
+    This function implements the calculation of the event specific AUC at time t for all times included in duration_col of pred_df.
+
+    Args:
+        pred_df (pd.DataFrame): Data to calculate prediction error.
+                                Must contain the observed duration and event-type, and the probability of event at time t prediction results for the event.
+                                See TwoStagesFitter.predict_prob_events()
+        event (int): Event-type to calculate the AUC for.
+        duration_col (str): Last follow up time column name (must be a column in pred_df).
+        event_type_col (str): The event type column name (must be a column in df),
+                              Right-censored sample (i) is indicated by event value 0, df.loc[i, event_type_col] = 0.
+    Returns:
+        result (pd.Series): event specific AUC for all times included in duration_col of pred_df.
+    """
+
+    res = {}
+    for t in sorted(pred_df[duration_col].unique())[:-1]:
+        res[t] = event_specific_auc_at_t(pred_df=pred_df, event=event, t=t,
+                                         event_type_col=event_type_col, duration_col=duration_col)
+    return pd.Series(res)
+
+
+def event_specific_auc_weights(pred_df: pd.DataFrame,
+                               event: int,
+                               event_type_col: str = 'J',
+                               duration_col: str = 'X') -> pd.Series:
+    """
+    This function implements the calculation of the event specific AUC weights.
+
+    Args:
+        pred_df (pd.DataFrame): Data to calculate prediction error.
+                                Must contain the observed duration and event-type, and the probability of event at time t prediction results for the event.
+                                See TwoStagesFitter.predict_prob_events()
+        event (int): Event-type to calculate the AUC weights for.
+        duration_col (str): Last follow up time column name (must be a column in pred_df).
+        event_type_col (str): The event type column name (must be a column in df),
+                              Right-censored sample (i) is indicated by event value 0, df.loc[i, event_type_col] = 0.
+    Returns:
+        result (pd.Series): event specific AUC weights.
+    """
+
+    kmf_overall = KaplanMeierFitter()
+    kmf_overall.fit(durations=pred_df[duration_col], event_observed=(pred_df[event_type_col] > 0))
+    kmf_event = KaplanMeierFitter()
+    kmf_event.fit(durations=pred_df[duration_col], event_observed=(pred_df[event_type_col] == event))
+    prob_jt = np.abs(-1*kmf_event.survival_function_.diff()[1:])  # abs to fix negative zero
+    overall_t_minus_1 = kmf_overall.survival_function_.iloc[:-1]
+    numer = (prob_jt.values.squeeze()) * (overall_t_minus_1.values.squeeze())
+    denom = (prob_jt.values.squeeze()).dot(overall_t_minus_1.values.squeeze())
+    result = pd.Series(numer / denom, index=prob_jt.index).iloc[:-1]
+    return result
+
+
+def event_specific_integrated_auc(pred_df: pd.DataFrame,
+                                  event: int,
+                                  event_type_col: str = 'J',
+                                  duration_col: str = 'X') -> float:
+    """
+    This function implements the calculation of the event specific integrated auc.
+
+    Args:
+        pred_df (pd.DataFrame): Data to calculate prediction error.
+                                Must contain the observed duration and event-type, and the probability of event at time t prediction results for the event.
+                                See TwoStagesFitter.predict_prob_events()
+        event (int): Event-type to calculate the integrated AUC for.
+        duration_col (str): Last follow up time column name (must be a column in pred_df).
+        event_type_col (str): The event type column name (must be a column in df),
+                              Right-censored sample (i) is indicated by event value 0, df.loc[i, event_type_col] = 0.
+    Returns:
+        result (float): integrated AUC results.
+    """
+
+    auc_at_t = event_specific_auc_at_t_all(pred_df=pred_df, event=event,
+                                           event_type_col=event_type_col,
+                                           duration_col=duration_col)
+    weights = event_specific_auc_weights(pred_df=pred_df, event=event,
+                                         event_type_col=event_type_col,
+                                         duration_col=duration_col)
+    result = auc_at_t.dot(weights)
+    return result
+
+
+def global_auc(pred_df: pd.DataFrame,
+               event_type_col: str = 'J',
+               duration_col: str = 'X') -> float:
+    """
+    This function implements the calculation of the global AUC.
+
+    Args:
+        pred_df (pd.DataFrame): Data to calculate prediction error.
+                                Must contain the observed duration and event-type, and the probability of event at time t prediction results for all events.
+                                See TwoStagesFitter.predict_prob_events()
+        duration_col (str): Last follow up time column name (must be a column in pred_df).
+        event_type_col (str): The event type column name (must be a column in df),
+                              Right-censored sample (i) is indicated by event value 0, df.loc[i, event_type_col] = 0.
+    Returns:
+        global_auc (float): global AUC results.
+    """
+
+    e_j_ser = pred_df.groupby('J').size().sort_index().iloc[1:]
+    total_e = e_j_ser.sum()
+    global_auc = 0
+    for event, e_j in e_j_ser.iteritems():
+        global_auc += (e_j / total_e) * event_specific_integrated_auc(
+            pred_df=pred_df, event=event, event_type_col=event_type_col,
+            duration_col=duration_col)
+    return global_auc
--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,67 +1,185 @@
 import unittest
+from src.pydts.data_generation import EventTimesSampler
+from src.pydts.evaluation import *
+from src.pydts.fitters import TwoStagesFitter
 import numpy as np
 import pandas as pd
-from src.pydts.data_generation import EventTimesSampler
-from src.pydts.evaluation import prediction_error
-from src.pydts.fitters import TwoStagesFitter
 
 
 class TestEvaluation(unittest.TestCase):
 
     def setUp(self):
-        n_cov = 15
-        beta1 = np.zeros(n_cov)
-        beta1[:5] = (-0.5 * np.log([0.8, 3, 3, 2.5, 2]))
-        beta2 = np.zeros(n_cov)
-        beta2[:5] = (-0.5 * np.log([1, 3, 4, 3, 2]))
+        n_cov = 5
+        beta1 = (-0.5 * np.log([0.8, 2, 3, 2.5, 1.2]))
+        beta2 = (-0.5 * np.log([1, 3, 2, 1.5, 2.7]))
 
-        real_coef_dict = {
+        self.real_coef_dict = {
             "alpha": {
-                1: lambda t: -2.0 - 0.2 * np.log(t),
-                2: lambda t: -2.2 - 0.2 * np.log(t)
+                1: lambda t: -2.8 - 0.1 * np.log(t),
+                2: lambda t: -3 - 0.1 * np.log(t)
             },
             "beta": {
                 1: beta1,
                 2: beta2
             }
         }
-        n_patients = 5000
+        n_patients = 15000
         d_times = 15
         j_events = 2
 
-        ets = EventTimesSampler(d_times=d_times, j_event_types=j_events)
+        self.ets = EventTimesSampler(d_times=d_times, j_event_types=j_events)
 
         seed = 0
         means_vector = np.zeros(n_cov)
         covariance_matrix = 0.5 * np.identity(n_cov)
         clip_value = 1
 
-        covariates = [f'Z{i + 1}' for i in range(n_cov)]
+        self.covariates = [f'Z{i + 1}' for i in range(n_cov)]
 
         patients_df = pd.DataFrame(data=pd.DataFrame(data=np.random.multivariate_normal(means_vector, covariance_matrix,
                                                                                         size=n_patients),
-                                                     columns=covariates))
+                                                     columns=self.covariates))
         patients_df.clip(lower=-1 * clip_value, upper=clip_value, inplace=True)
-        patients_df = ets.sample_event_times(patients_df, hazard_coefs=real_coef_dict, seed=seed)
+        patients_df = self.ets.sample_event_times(patients_df,
+                                                  hazard_coefs=self.real_coef_dict,
+                                                  seed=seed)
         patients_df.index.name = 'pid'
         patients_df = patients_df.reset_index()
-        patients_df = ets.sample_independent_lof_censoring(patients_df, prob_lof_at_t=0.01 * np.ones_like(ets.times))
-        self.patients_df = ets.update_event_or_lof(patients_df)
+        patients_df = self.ets.sample_independent_lof_censoring(patients_df,
+                                                                prob_lof_at_t=0.01 * np.ones_like(self.ets.times))
+        self.patients_df = self.ets.update_event_or_lof(patients_df)
 
     def test_prediction_error(self):
-        L1_regularized_fitter = TwoStagesFitter()
+        fitter = TwoStagesFitter()
+        fitter.fit(df=self.patients_df.drop(['C', 'T'], axis=1), nb_workers=1)
+        pred_df = fitter.predict_cumulative_incident_function(self.patients_df)
+        prediction_error(pred_df)
 
-        fit_beta_kwargs = {
-            'model_kwargs': {
-                'penalizer': 0.01,
-                'l1_ratio': 1
-            }
-        }
+    def test_event_specific_prediction_error(self):
+        fitter = TwoStagesFitter()
+        fitter.fit(df=self.patients_df.drop(['C', 'T'], axis=1), nb_workers=1)
+        pred_df = fitter.predict_cumulative_incident_function(self.patients_df)
+        event_specific_prediction_error(pred_df, event=1)
 
-        L1_regularized_fitter.fit(df=self.patients_df.drop(['C', 'T'], axis=1),
-                                  fit_beta_kwargs=fit_beta_kwargs,
-                                  nb_workers=1)
+    def test_event_specific_auc(self):
+        fitter = TwoStagesFitter()
+        fitter.fit(df=self.patients_df.drop(['C', 'T'], axis=1), nb_workers=1)
+        pred_df = fitter.predict_prob_event_j_all(self.patients_df, event=1)
+        esauc = event_specific_auc_at_t(pred_df, event=1, t=9)
+        print(esauc)
 
-        pred_df = L1_regularized_fitter.predict_cumulative_incident_function(self.patients_df)
-        pe = prediction_error(pred_df)
-        print(pe)
+    def test_event_specific_auc_perfect_model(self):
+        cov_df = self.patients_df[self.covariates]
+        hazards = self.ets.calculate_hazards(cov_df, self.real_coef_dict, events=self.ets.events)
+        overall_survival = self.ets.calculate_overall_survival(hazards)
+        probs_j_at_t = self.ets.calculate_prob_event_at_t(hazards, overall_survival)
+        event = 1
+        probs_j_at_t = pd.DataFrame(probs_j_at_t[event-1].values,
+                                    columns=[f'prob_j{event}_at_t{t}'
+                                             for t in range(1, self.ets.d_times+1)])
+        pred_df = pd.concat([self.patients_df, probs_j_at_t], axis=1)
+        esauc = event_specific_auc_at_t(pred_df, event=event, t=9)
+        print(esauc)
+
+    def test_event_specific_auc_all_perfect_model(self):
+        cov_df = self.patients_df[self.covariates]
+        hazards = self.ets.calculate_hazards(cov_df, self.real_coef_dict, events=self.ets.events)
+        overall_survival = self.ets.calculate_overall_survival(hazards)
+        probs_j_at_t = self.ets.calculate_prob_event_at_t(hazards, overall_survival)
+        event = 1
+        probs_j_at_t = pd.DataFrame(probs_j_at_t[event-1].values,
+                                    columns=[f'prob_j{event}_at_t{t}'
+                                             for t in range(1, self.ets.d_times+1)])
+        pred_df = pd.concat([self.patients_df, probs_j_at_t], axis=1)
+        esauc = event_specific_auc_at_t_all(pred_df, event=event)
+        print(esauc)
+
+    def test_event_specific_integrated_auc_perfect_model(self):
+        cov_df = self.patients_df[self.covariates]
+        hazards = self.ets.calculate_hazards(cov_df, self.real_coef_dict, events=self.ets.events)
+        overall_survival = self.ets.calculate_overall_survival(hazards)
+        probs_j_at_t = self.ets.calculate_prob_event_at_t(hazards, overall_survival)
+        event = 1
+        probs_j_at_t = pd.DataFrame(probs_j_at_t[event-1].values,
+                                    columns=[f'prob_j{event}_at_t{t}'
+                                             for t in range(1, self.ets.d_times+1)])
+        pred_df = pd.concat([self.patients_df, probs_j_at_t], axis=1)
+        esauc = event_specific_integrated_auc(pred_df, event=event)
+        print(esauc)
+
+    def test_global_auc_perfect_model(self):
+        cov_df = self.patients_df[self.covariates]
+        hazards = self.ets.calculate_hazards(cov_df, self.real_coef_dict, events=self.ets.events)
+        overall_survival = self.ets.calculate_overall_survival(hazards)
+        probs_j_at_t = self.ets.calculate_prob_event_at_t(hazards, overall_survival)
+        event = 1
+        probs_j_at_t_1 = pd.DataFrame(probs_j_at_t[event-1].values,
+                                    columns=[f'prob_j{event}_at_t{t}'
+                                             for t in range(1, self.ets.d_times+1)])
+        event = 2
+        probs_j_at_t_2 = pd.DataFrame(probs_j_at_t[event-1].values,
+                                    columns=[f'prob_j{event}_at_t{t}'
+                                             for t in range(1, self.ets.d_times+1)])
+        pred_df = pd.concat([self.patients_df, probs_j_at_t_1, probs_j_at_t_2], axis=1)
+        esauc = global_auc(pred_df)
+        print(esauc)
+
+    def test_event_specific_weights_perfect_model(self):
+        cov_df = self.patients_df[self.covariates]
+        hazards = self.ets.calculate_hazards(cov_df, self.real_coef_dict, events=self.ets.events)
+        overall_survival = self.ets.calculate_overall_survival(hazards)
+        probs_j_at_t = self.ets.calculate_prob_event_at_t(hazards, overall_survival)
+        event = 1
+        probs_j_at_t = pd.DataFrame(probs_j_at_t[event-1].values,
+                                    columns=[f'prob_j{event}_at_t{t}'
+                                             for t in range(1, self.ets.d_times+1)])
+        pred_df = pd.concat([self.patients_df, probs_j_at_t], axis=1)
+        weights = event_specific_auc_weights(pred_df, event=event)
+        print(weights)
+
+    def test_event_specific_auc_simple_case_j1_t5(self):
+        # d=6 times, n=7, J=2
+        d = 6
+        pred_df = pd.DataFrame(data=[
+            [1, 3, 0.1, 0.1, 0.1, 0.07, 0.07, 0.01],
+            [1, 5, 0.05, 0.05, 0.05, 0.07, 0.1, 0.15],
+            [0, 7, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+            [0, 2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+            [0, 7, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+            [2, 4, 0.04, 0.04, 0.04, 0.07, 0.08, 0.1],
+            [2, 6, 0.04, 0.04, 0.07, 0.07, 0.12, 0.12],
+        ], columns=['J', 'X'] + [f'prob_j1_at_t{t}' for t in range(1, d+1)])
+        esauc = event_specific_auc_at_t(pred_df, event=1, t=5)
+        assert esauc == 0.5
+
+    def test_event_specific_auc_simple_case_j1_t3(self):
+        # d=6 times, n=7, J=2
+        d = 6
+        pred_df = pd.DataFrame(data=[
+            [1, 3, 0.1, 0.1, 0.1, 0.07, 0.07, 0.01],
+            [1, 5, 0.05, 0.05, 0.05, 0.07, 0.1, 0.15],
+            [0, 7, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+            [0, 2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+            [0, 7, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+            [2, 4, 0.04, 0.04, 0.04, 0.07, 0.08, 0.1],
+            [2, 6, 0.04, 0.04, 0.07, 0.07, 0.12, 0.12],
+        ], columns=['J', 'X'] + [f'prob_j1_at_t{t}' for t in range(1, d+1)])
+        esauc = event_specific_auc_at_t(pred_df, event=1, t=3)
+        assert esauc == 0.9
+
+    def test_cause_specific_auc_weights(self):
+        # d=6 times, n=7, J=2
+        d = 6
+        pred_df = pd.DataFrame(data=[
+            [0, 1, 0.1, 0.1, 0.1, 0.07, 0.07, 0.01],
+            [1, 3, 0.1, 0.1, 0.1, 0.07, 0.07, 0.01],
+            [1, 5, 0.05, 0.05, 0.05, 0.07, 0.1, 0.15],
+            [0, 7, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+            [0, 2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+            [0, 7, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+            [2, 4, 0.04, 0.04, 0.04, 0.07, 0.08, 0.1],
+            [2, 6, 0.04, 0.04, 0.07, 0.07, 0.12, 0.12],
+        ], columns=['J', 'X'] + [f'prob_j1_at_t{t}' for t in range(1, d+1)])
+
+        event_specific_auc_weights(pred_df, event=1)
+        print('x')
